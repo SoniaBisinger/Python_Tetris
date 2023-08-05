b6c7a0ac46eacf333a366c1e87575c05308149c1
Sonia Bisinger <sonia.bisinger@gmail.com>
2023-08-05 09:55:48 +0200

    created greed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from grid import Grid
 
 pygame.init
 
@@ -6,6 +7,9 @@ screen = pygame.display.set_mode((300, 600))
 pygame.display.set_caption("Python Tetris")
 
 clock = pygame.time.Clock()
+
+game_grid = Grid()
+game_grid.print_grid()
 
 # Game Loop
 # 1. Event Handling
@@ -18,5 +22,6 @@ while True:
             pygame.quit()
             SystemExit()
 
+    screen.fill("pink")
     pygame.display.update()
     clock.tick(60) #code will run 60 times a second
