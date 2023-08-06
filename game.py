91efdac7f97b06a4936860586210fe45c1cb55e0
Sonia Bisinger <sonia.bisinger@gmail.com>
2023-08-06 11:37:48 +0200

    Completed row goes away

--- game.py
+++ game.py
@@ -19,12 +19,12 @@ class Game:
 
     def move_left(self):
         self.current_block.move(0, -1)
-        if self.block_inside() == False:
+        if self.block_inside() == False or self.block_fits() == False:
             self.current_block.move(0, 1)
 
     def move_right(self):
         self.current_block.move(0, 1)
-        if self.block_inside() == False:
+        if self.block_inside() == False or self.block_fits() == False:
             self.current_block.move(0, -1)
 
     def move_down(self):
@@ -39,10 +39,11 @@ class Game:
             self.grid.grid[position.row][position.column] = self.current_block.id
         self.current_block = self.next_block
         self.next_block = self.get_random_block()
+        self.grid.clear_full_rows()
 
     def rotation(self):
         self.current_block.rotate()
-        if self.block_inside == False:
+        if self.block_inside == False or self.block_fits() == False:
             self.current_block.undo_rotation()
 
     def block_inside(self):
--- grid.py
+++ grid.py
@@ -25,6 +25,33 @@ class Grid:
             return True
         return False
 
+    def is_row_full(self, row):
+        for column in range(self.num_cols):
+            if self.grid[row][column] == 0:
+                return False
+        return True
+
+    def clear_row(self, row):
+        for column in range(self.num_cols):
+            self.grid[row][column] = 0
+
+    def move_row_down(self, row, num_rows):
+        for column in range(self.num_cols):
+            self.grid[row+num_rows][column] = self.grid[row][column]
+            self.grid[row][column] = 0
+
+    def clear_full_rows(self):
+        completed = 0
+        #need check every rows
+        for row in range(self.num_rows-1, 0, -1): #checking from last row to the first one, 1 by 1
+            if self.is_row_full(row):
+                self.clear_row(row)
+                completed += 1
+            elif completed > 0:
+                self.move_row_down(row, completed)
+        return completed
+
+
     def draw(self, screen):
         # dark krey if cell = 0, green if cell = 1 etc
         for row in range(self.num_rows):
